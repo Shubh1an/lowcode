e5c24e00a3b380a5865da3991ad35d4e696d367a
Gopala Khore <gopalakhoria@gmail.com>
2024-05-09 16:40:36 +0530

    Code cleanup

--- src/App.js
+++ src/App.js
@@ -1,30 +1,14 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Modules from "./Module";
-import Entity from "./Entity";
-import FormBuilder from "./Form";
-import FillForm from "./FormFill";
-import ViewFormData from "./ViewForm";
 import Layout from "./Layout/Layout";
-import Listview from "./Components/ListView";
 function App() {
   return (
     <div className="App">
-      
       <Layout>
-      <Routes>
-      <Route path="/" element={<Listview />} />
-          {/* <Route path="" element={<Modules />} />
-          <Route path="/modules" element={<Modules />} />
-          <Route path="/entity" element={<Entity />} />
-          <Route path="/form" element={<FormBuilder />} />
-          <Route path="/fill-form" element={<FillForm />} />
-          <Route path="/view-form" element={<ViewFormData />} /> */}
+        <Routes>
+          <Route path="/" element={<div />} />
         </Routes>
       </Layout>
-      {/* <BrowserRouter>
-       
-      </BrowserRouter> */}
     </div>
   );
 }
---
+++ src/Layout/Sidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div className='h-screen w-1/6 bg-slate-200'>
+
+    </div>
+  )
+}
+
+export default Sidebar
