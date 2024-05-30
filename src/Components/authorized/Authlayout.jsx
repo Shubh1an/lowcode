d4319229e95bcb47f585e69beb89b167da1b8501
anjali459 <adubey@moreyeahs.in>
2024-05-30 19:06:33 +0530

    auth code fro frontend

--- src/Components/authorized/Authlayout.jsx
+++ src/Components/authorized/Authlayout.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from './Home';
-import Layout from './layout';
+import Layout from './Layout';
 
 const Authlayout = () => {
   return (
     <div>
       <Routes>
-        <Layout>
-          <Route path="/auth/home" element={<Home />} />
-        </Layout>
+        <Route
+          path="/auth/home"
+          element={
+            <Layout>
+              <Home />
+            </Layout>
+          }
+        />
       </Routes>
     </div>
   );
