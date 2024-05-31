8bbd80bcd00bb4c8f791bb0000065a3f02b72d59
anjali459 <adubey@moreyeahs.in>
2024-05-31 18:05:28 +0530

    frontend

--- src/Config/config.js
+++ src/Config/config.js
@@ -1,7 +1,7 @@
 const config = {
   // BASE_URL: 'https://45r45nns-5000.inc1.devtunnels.ms/api',
-  BASE_URL: 'http://localhost:5002/api',
-  // BASE_URL: 'http://192.168.1.50:9966/api',
+  // BASE_URL: 'http://localhost:5002/api',
+  BASE_URL: 'http://192.168.1.50:9968/api',
 };
 
 export default config;
